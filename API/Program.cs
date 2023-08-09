43e1d6cf9c5da01a7f12ce18319e0b8f667ef6b2
Vali Izzatullaev <valiizzatullayev@gmail.com>
2023-08-09 11:11:55 +0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 app.Run();
